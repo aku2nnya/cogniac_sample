a474956a8d23769c761662a081f68291aff89a16
Toshihiro Akabori <toshihiro.akabori@yahoo.com>
2019-06-09 12:20:14 -0700

    Implement API setup

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,12 +1,29 @@
 import React from 'react';
+import axios from 'axios';
 
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      data: {},
     };
+    this.getData = this.getData.bind(this);
+  }
+
+  componentDidMount() {
+    this.getData();
+  }
+
+  getData() {
+    axios.get('/data')
+      .then((response) => {
+        this.setState({
+          data: response.data,
+        });
+      })
+      .then(() => console.log(this.state.data))
+      .catch(error => console.log(error));
   }
 
   render() {
